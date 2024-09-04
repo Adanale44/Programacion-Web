7b8aa5119e577255219d97d120f46198aae5373f
adan <dbsyc1214@.com>
2024-09-04 16:58:25 -0300

    Hice el blog personal

---
+++ Blog Personal/vite-project/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <p>© 2024 Mi Blog Personal. Todos los derechos reservados.</p>
+        <div className="social-media">
+          <a> Mis contactos</a>
+          <a href="https://twitter.com" target="_blank" >Twitter</a>
+          <a href="https://facebook.com" target="_blank" >Facebook</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Blog Personal/vite-project/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <h1>Mi Blog Personal</h1>
+        <div>
+          <a>Inicio</a>
+          <a>Sobre Mí</a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
